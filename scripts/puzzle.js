be3f48e00af37171f14c725f4f0981de080ba1cc
John Kraus <john@johnfkraus.com>
2019-11-13 00:55:58 -0500

    added license

--- scripts/puzzle.js
+++ scripts/puzzle.js
@@ -2,7 +2,12 @@
 // Ajax-JavaScript-CSS Puzzle by John Kraus
 // john@johnfkraus.com johnkraus3@gmail.com www.johnfkraus.com
 // October 2009, December 2012, March 2013
-
+/*
+Copyright (C) 2019 John F. Kraus
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 // ============
 // SOLVE PUZZLE
